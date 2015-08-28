336ab8bd08a00840714a0c3a83a0085b017d0e8e
askl56 <amscott@openmailbox.org>
2015-08-28 16:26:23 +0100

    added animal model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828152436) do
+ActiveRecord::Schema.define(version: 20150828152537) do
+
+  create_table "animals", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "race"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tribes", force: :cascade do |t|
     t.string   "name"
