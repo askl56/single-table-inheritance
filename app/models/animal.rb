7939860046bb493cf436911c3cea47697c814b59
askl56 <amscott@openmailbox.org>
2015-09-07 08:59:37 +0100

    implemented animal and tribe models

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,10 @@
 class Animal < ActiveRecord::Base
   belongs_to :tribe
-  self.inheritance_column = :race
+  inheritance_column = :race
+
+  scope :lions, -> { where(race: 'Lion') }
+  scope :meerkats, -> { where(race: 'Meerkat') }
+  scope :wild_boars, -> { where(race: 'WildBoar') }
 
 
   def self.races
