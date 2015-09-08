57ff3f25a996b7d415d5a8d9474fa86c47f71ce6
askl56 <amscott@openmailbox.org>
2015-09-08 09:17:51 +0100

    added animals controller and animals index view

---
+++ app/controllers/animals_controller_controller.rb
@@ -0,0 +1,23 @@
+class AnimalsControllerController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
