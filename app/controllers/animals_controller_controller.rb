447a8ae29b98d7fc3999db011aba91a9cc5b7de2
askl56 <amscott@openmailbox.org>
2015-09-08 09:27:25 +0100

    updated controller

--- app/controllers/animals_controller_controller.rb
+++ app/controllers/animals_controller_controller.rb
@@ -1,23 +1,46 @@
 class AnimalsControllerController < ApplicationController
+
+  before_action :set_race
+
   def index
-    @animals = Animal.all
+    @animals = race_class.all
   end
 
   def show
+
   end
 
   def new
+
   end
 
   def edit
+
   end
 
   def create
+
   end
 
   def update
+
   end
 
   def destroy
+
+  end
+
+  private
+
+  def set_race
+    @race = race
+  end
+
+  def race
+    Animal.races.include?(params[:type]) ? params[:type] : "Animal"
+  end
+
+  def race_class
+    race.constantize
   end
 end
